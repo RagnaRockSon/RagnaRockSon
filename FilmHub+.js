6d4f1fc74406f14bb3d62f91e885c0e23227232c
RagnaRockSon <strelockroland@gmail.com>
2026-02-26 20:52:04 +0200

    Update FilmHub+.js

--- FilmHub+.js
+++ FilmHub+.js
@@ -1,39 +1,40 @@
 (function() {
     'use strict';
 
-    var VERSION = '1.0';
     var PLUGIN_NAME = 'FilmHub+';
 
-    // Повідомлення про завантаження плагіну
-    if (Lampa.Noty) Lampa.Noty.show(`Плагін ${PLUGIN_NAME} ${VERSION} завантажено`);
+    function addFilmHubButton(card, movie) {
+        if (card.find('.filmhub-button').length) return;
 
-    // Додаємо кнопку в картку фільму
-    function addFilmHubButton(e) {
-        if (e.render.find('.filmhub-button').length) return;
+        // Створюємо кнопку з іконкою 🎬
+        var btn = $(`
+            <div class="lampac--button filmhub-button" 
+                 style="display:flex; align-items:center; background:#ff5722; font-weight:bold; padding:0.5em 1em; border-radius:0.5em; margin-right:0.5em;">
+                <span style="font-size:1.5em; margin-right:0.5em;">🎬</span>
+                ${PLUGIN_NAME}
+            </div>
+        `);
 
-        var btn = $('<div class="lampac--button filmhub-button">FilmHub+</div>');
         btn.on('hover:enter', function() {
-            Lampa.Noty.show('Відкриваємо FilmHub+');
-            openFilmHub(e.object);
+            Lampa.Noty.show(`Відкриваємо FilmHub+ для "${movie.title || movie.name}"`);
+            openFilmHub(movie);
         });
 
-        // Додаємо кнопку в меню поруч з існуючими
-        e.render.find('.card-buttons').prepend(btn);
+        // Додаємо кнопку ліворуч у контейнер кнопок
+        var container = card.find('.card-buttons');
+        if (container.length) container.prepend(btn);
+        else card.append(btn);
     }
 
-    // Основна логіка плагіну
     function openFilmHub(movie) {
-        // Тут пізніше підключимо джерела і плейлисти
         console.log('FilmHub+ відкрито для фільму:', movie);
         Lampa.Noty.show(`Фільм: ${movie.title || movie.name}`);
+        // Тут буде логіка відкриття нашого плагіна
     }
 
-    // Реєструємо плагін у Лампі
-    Lampa.Component.add(PLUGIN_NAME, function(object) {
-        return {
-            initialize: function() {
-                addFilmHubButton({ render: object.render, object: object.movie });
-            }
-        }
+    // Слухаємо рендер картки фільму
+    Lampa.Listener.follow('card:render', function(card, movie) {
+        addFilmHubButton(card, movie);
     });
+
 })();
